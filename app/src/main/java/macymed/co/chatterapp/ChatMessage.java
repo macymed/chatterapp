ac845133ce9e24a152167c5a589e841ecc1a4ed4
Macy Medema <macymedema@gmail.com>
2017-03-15 17:17:12 -0700

    first commit

---
+++ app/src/main/java/macymed/co/chatterapp/ChatMessage.java
@@ -0,0 +1,30 @@
+package macymed.co.chatterapp;
+
+/**
+ * Created by macymedema on 1/19/17.
+ */
+import java.util.Random;
+
+public class ChatMessage {
+
+    public String body, sender, receiver, senderName;
+    public String Date, Time;
+    public String msgid;
+    public boolean isMine;// Did I send the message.
+
+    public ChatMessage(String Sender, String Receiver, String messageString,
+                       String ID, boolean isMINE) {
+        body = messageString;
+        isMine = isMINE;
+        sender = Sender;
+        msgid = ID;
+        receiver = Receiver;
+        senderName = sender;
+    }
+
+    public void setMsgID() {
+
+        msgid += "-" + String.format("%02d", new Random().nextInt(100));
+        ;
+    }
+}

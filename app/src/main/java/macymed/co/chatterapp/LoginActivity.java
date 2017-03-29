9e937dba46abb5bf86c696007f9a5e681d7738e0
Macy Medema <macymedema@gmail.com>
2017-03-29 16:09:37 -0700

    added form validation on registration

--- app/src/main/java/macymed/co/chatterapp/LoginActivity.java
+++ app/src/main/java/macymed/co/chatterapp/LoginActivity.java
@@ -1,7 +1,10 @@
 package macymed.co.chatterapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button registerButton = (Button) findViewById(R.id.register_button);
+        Button loginButton = (Button) findViewById(R.id.login_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent reg = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(reg);
+            }
+        });
+
     }
 }
--- app/src/main/java/macymed/co/chatterapp/RegisterActivity.java
+++ app/src/main/java/macymed/co/chatterapp/RegisterActivity.java
@@ -2,12 +2,83 @@ package macymed.co.chatterapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.util.List;
+
+
+public class RegisterActivity extends AppCompatActivity implements Validator.ValidationListener, View.OnClickListener {
+    @NotEmpty
+    private EditText userName;
+
+    @NotEmpty
+    @Email
+    private EditText email;
+
+    @NotEmpty
+    @Password(min = 8, scheme = Password.Scheme.ALPHA_NUMERIC_MIXED_CASE_SYMBOLS)
+    EditText pass;
+
+    @ConfirmPassword
+    EditText confirmPass;
+
+
+    Button registerButton;
+    private Validator validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerButton = (Button) findViewById(R.id.registerButton);
+        userName = (EditText) findViewById(R.id.userName);
+        email = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.password);
+        confirmPass = (EditText) findViewById(R.id.confirmPass);
+
+
+
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+
+        registerButton.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.registerButton:
+                validator.validate();
+                break;
+        }
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        Toast.makeText(RegisterActivity.this, "valid success", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+            if(view instanceof EditText){
+                ((EditText) view).setError(message);
+            }else{
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
